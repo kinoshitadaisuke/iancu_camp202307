435740811f9916c7f065953fe3c4a79f5644a408
Daisuke <mhdf23888@gmail.com>
2023-07-26 13:52:56 +0800

    adding camp2023_s01_00.py

---
+++ s01/camp2023_s01_00.py
@@ -0,0 +1,8 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/07/26 13:43:04 (CST) daisuke>
+#
+
+# printing a sentence using a function "print ()"
+print (f'This is my very first Python script!')
