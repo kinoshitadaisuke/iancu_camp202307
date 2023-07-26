7c3131ec4fb3e0554e348047e9ee03042c728607
Daisuke <mhdf23888@gmail.com>
2023-07-26 18:53:13 +0800

    adding s01/camp2023_s01_01.py

---
+++ s01/camp2023_s01_01.py
@@ -0,0 +1,19 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/07/26 18:38:44 (CST) daisuke>
+#
+
+# variable "a"
+a = 17.56
+
+# variable "b"
+b = 12.34
+
+# calculation of c = a - b
+c = a - b
+
+# printing result
+print (f'a = {a}')
+print (f'b = {b}')
+print (f'c = {a} - {b} = {c}')
